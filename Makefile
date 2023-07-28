09ea6dce253ad4b1bfeee224f5927dc06ab5fe17
Dave <dblencowe@gmail.com>
2023-07-28 23:16:09 +0100

    initial commit

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: up guard-%
+
+up: guard-USER_NAME guard-GIT_USER_NAME guard-GIT_EMAIL
+	ansible-playbook playbook.yml --extra-vars="user_name=${USER_NAME} user_git_name=${GIT_USER_NAME} user_email=${USER_EMAIL}" --ask-become-pass
+
+guard-%:
+    @ if [ "${${*}}" = "" ]; then \
+        echo "Environment variable $* not set"; \
+        exit 1; \
+    fi
---
+++ aur/install-aur.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+
+searchQuery=${1}
+if [ ! -z "${2}" ]; then
+  searchQuery="${1} ${2}"
+fi
+
+searchResult=`pacman -Q | grep -sw "${searchQuery}"`
+# Only install if package does not exist
+if [ -z "${searchResult}" ]; then
+  echo "Installing ${searchQuery} package"
+  # Download Arch linux AUR package
+  wget https://aur.archlinux.org/cgit/aur.git/snapshot/${1}.tar.gz -P /tmp/${1}
+
+  # Extract package
+  tar -xvzf /tmp/${1}/${1}.tar.gz -C /tmp/${1}/
+
+  # Create install package
+  cd /tmp/${1}/${1}
+  makepkg -s --skippgpcheck
+
+  # Install package
+  makepkg -si --noconfirm
+
+  # Remove temporary folder
+  rm -rf /tmp/${1}
+else
+  echo "${searchQuery} package already installed"
+  exit -1;
+fi
